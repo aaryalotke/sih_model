12cb5a0404f0020b2f64d872685db1c92a9c253d
Aaman <aaman.bhowmick21@gmail.com>
2023-12-20 15:03:41 +0530

    Hogaya

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import FixedCost from './FixedCost';
-import FixedMonthly from '../Components/FixedMonthly';
+import React, { useState } from "react";
+import FixedCost from "./FixedCost";
+import FixedMonthly from "../Components/FixedMonthly";
 
 const Profile = () => {
   const [restaurantData, setRestaurantData] = useState({
-    name: '',
-    location: '',
-    email: '',
-    phone: '',
+    name: "",
+    location: "",
+    email: "",
+    phone: "",
   });
 
   const spacing = 2;
@@ -25,7 +25,6 @@ const Profile = () => {
       <section className="text-gray-600 body-font relative">
         <div className="container px-5 py-4 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-
             <iframe
               width="100%"
               height="100%"
@@ -38,8 +37,8 @@ const Profile = () => {
               src={`https://maps.google.com/maps?q=${encodeURIComponent(
                 restaurantData.location
               )}&output=embed`}
-              style={{ marginRight: spacing + 'em' }}
-              ></iframe>
+              style={{ marginRight: spacing + "em" }}
+            ></iframe>
 
             <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
               <div className="lg:w-1/2 px-6">
@@ -59,15 +58,13 @@ const Profile = () => {
                   PHONE
                 </h2>
                 <p className="leading-relaxed">{restaurantData.phone}</p>
-
-
               </div>
             </div>
           </div>
           <div className="bg-gray-100 min-h-screen py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden">
               <div className="p-6">
-                <h2 className="text-xl font-semibold text-gray-800 mb-4">
+                <h2 className="text-3xl font-semibold mb-4 text-[#272635]">
                   Restaurant Profile
                 </h2>
                 <div className="mb-4">
@@ -80,7 +77,7 @@ const Profile = () => {
                     className="text-gray-800 font-semibold w-full border-b border-gray-300 focus:outline-none focus:border-indigo-500"
                   />
                 </div>
-                
+
                 <div className="mb-4">
                   <h3 className="text-gray-600 text-sm">Location</h3>
                   <input
@@ -113,9 +110,9 @@ const Profile = () => {
                 </div>
               </div>
 
-              <FixedMonthly/>
-              <div className="bg-gray-100 p-4 flex justify-end">
-                <button className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600">
+              <FixedMonthly />
+              <div className="bg-gray-100 p-4 flex justify-around">
+                <button className="bg-[#272635] text-white py-2 px-4 rounded-md hover:bg-[#000]">
                   Edit Profile
                 </button>
                 <button className="bg-green-500 text-white py-2 px-4 ml-2 rounded-md hover:bg-green-600">
